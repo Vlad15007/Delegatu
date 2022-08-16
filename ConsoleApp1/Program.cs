7bb10577cfe8ed194ac7e101f58ba44754bafba8
my comp <my comp@DESKTOP-D6ORP48>
2022-08-16 11:09:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sreda myAkvarium = new Sreda(new Kletko("Животная"), new Kletko("Растительная"), new Kletko("Бактериальная"));
+            myAkvarium.Jut();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Sreda
+    {
+        Kletko[] kletki;
+        public void Jut()
+        {
+            foreach (Kletko kletko in kletki)
+            {
+                Console.Write(kletko.GetType().Name + " - ");
+                //kletko.Syshestvovat();
+                kletko.sposobnosti();
+                Console.WriteLine();
+            }
+        }
+        public Sreda(params Kletko[] kletki)
+        {
+            this.kletki = kletki;
+        }
+    }
+
+    public delegate void SposobnostiKletki();
+
+    class Kletko
+    {
+        string type;
+        public SposobnostiKletki sposobnosti;
+
+        void Delenie()
+        {
+            Console.WriteLine("Деление");
+        }
+
+        void Fotosintez()
+        {
+            Console.WriteLine("Фотосинтез");
+        }
+
+        void Dvigatsa()
+        {
+            Console.WriteLine("Двигаюсь");
+        }
+
+        void Prikreplatsa()
+        {
+            Console.WriteLine("Прикрепляюсь");
+        }
+
+        void Pogloshat()
+        {
+            Console.WriteLine("Поглощяю");
+        }
+
+        public Kletko(string type)
+        {
+            sposobnosti = new SposobnostiKletki(Delenie);
+
+            if(type == "Животная")
+            {
+                sposobnosti += Pogloshat;
+            }   
+            else if(type == "Растительная")
+            {
+                sposobnosti += Fotosintez;
+            }    
+            else if(type == "Бактериальная")
+            {
+                sposobnosti += Dvigatsa;
+                sposobnosti += Prikreplatsa;
+                sposobnosti += Pogloshat;
+            }
+            this.type = type;
+        }
+
+        public void Syshestvovat()
+        {
+            Console.WriteLine(type);
+            sposobnosti();
+        }
+    }
+}
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public delegate void AccauntStory();
+
+    public partial class Form1 : Form
+    {
+        Accaunt myAcc;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AccauntStory myZapis = delegate
+            {
+                label1.Text = myAcc.Schet + "UAN";
+                textBox1.Text += myAcc.Schet + "\r\n";
+            };
+
+
+            myAcc = new Accaunt(myZapis);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myAcc.Popolnit(1000);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myAcc.Snatie(200);
+        }
+    }
+
+    class Accaunt
+    {
+        AccauntStory show;
+        double schet = 1000;
+        public double Schet 
+        {
+            get
+            {
+                return schet;
+            }
+        }
+
+        public void Popolnit(double symma)
+        {
+            schet += symma;
+            show();
+        }
+        public void Snatie(double symma)
+        {
+            schet -= symma;
+            show();
+        }
+
+        public Accaunt(AccauntStory zapisuvanie)
+        {
+            show = zapisuvanie;
+        }
+    }
+}
